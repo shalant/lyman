3ebbbcec9a6130155486b07f7701a9cbe64e4924
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-05-13 21:10:45 -0500

    got halfway through module 4...uh...whoa

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lyman918a3823": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
